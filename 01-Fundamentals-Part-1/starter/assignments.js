7fba3a19214fbb4043d0d91e7707a84ebc53b7cb
Charles Aggasid <charles.aggasid01@gmail.com>
2022-05-17 08:55:47 -0500

    assignments

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+let country = 'Philippines';
+const continent = 'Asia';
+let population = 100000000;
+
+const isIsland = false;
+const language = 'Tagalog'
+
+console.log(country, continent, population, isIsland, language)
+
+
+let x = 1;
+
+x++;
+
+console.log(x)
